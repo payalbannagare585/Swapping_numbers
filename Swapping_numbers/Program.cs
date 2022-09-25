a5961fdc7388b047632cc30c349f8b8ae311b5fe
Gaurav <Gaurav@LAPTOP-TREG306B>
2022-09-25 20:07:31 +0530

    Add project files.

---
+++ Swapping_numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapping_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b;
+
+            Console.WriteLine("enter value of a: ");
+            a=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter value of b: ");
+            b=Convert.ToInt32(Console.ReadLine());  
+
+            //swapping
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("Values after swapping are:");
+            Console.WriteLine("a=" + a);
+            Console.WriteLine("b=" + b);
+        }
+    }
+}
